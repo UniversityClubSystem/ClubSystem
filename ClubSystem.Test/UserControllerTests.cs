a39cf5eaf63a4c374ac2b4a63a60d5b873828606
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2018-11-28 00:23:33 +0300

    test

--- ClubSystem.Test/UserControllerTests.cs
+++ ClubSystem.Test/UserControllerTests.cs
@@ -7,6 +7,7 @@ using Xunit;
 using ClubSystem.Lib.Model.User;
 using Microsoft.Extensions.Options;
 using System;
+using ClubSystem.Api.Extensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Authentication;
@@ -15,39 +16,25 @@ namespace ClubSystem.Test
 {
     public class UserControllerTests
     {
-        private Mock<FakeUserManager> _mockUserManager;
-        private Mock<FakeSignInManager> _mockSignInManager;
-        private UserController _userController;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly JwtTokenGenerator _jwtTokenGenerator;
+        private readonly UserController _userController;
 
-        public UserControllerTests()
+        public UserControllerTests(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration)
         {
-            _mockUserManager = new Mock<FakeUserManager>();
-            _mockSignInManager = new Mock<FakeSignInManager>();
+            _userManager = userManager;
+            _signInManager = signInManager;
+            
+            _jwtTokenGenerator = new JwtTokenGenerator(configuration);
+            _userController = new UserController(userManager, signInManager, configuration);
         }
 
         [Fact]
         public async void UserCanLogin()
         {
             // Arrange
-            var userStoreMock = new Mock<IUserStore<ApplicationUser>>(); // ApplicationUser => User
-
-            _mockUserManager = new Mock<UserManager<ApplicationUser>>(userStoreMock.Object,
-                null, null, null, null, null, null, null, null);
-
-            var contextAccessor = new Mock<IHttpContextAccessor>();
-            var userPrincipalFactory = new Mock<IUserClaimsPrincipalFactory<User>>();
-
-            _mockSignInManager = new Mock<SignInManager<ApplicationUser>>(_mockUserManager.Object,
-                contextAccessor.Object, userPrincipalFactory.Object, null, null, null);
-
-
-            var mockConfiguration = new Mock<IConfiguration>();
-            var mockUser = new User
-            {
-                UserName = "user1",
-                Email = "mail@mail.com",
-                Password = "Ultra_Secure2Password!"
-            };
+            var mockUser = new User { UserName = "user1", Email = "mail@mail.com", Password = "Ultra_Secure2Password!" };
             
             // Act
             var result = await _userController.Login(mockUser);
@@ -57,32 +44,32 @@ namespace ClubSystem.Test
         }
     }
 
-    public class FakeUserManager : UserManager<ApplicationUser>
-    {
-        public FakeUserManager() : base(new Mock<IUserStore<ApplicationUser>>().Object,
-                  new Mock<IOptions<IdentityOptions>>().Object,
-                  new Mock<IPasswordHasher<ApplicationUser>>().Object,
-                  new IUserValidator<ApplicationUser>[0],
-                  new IPasswordValidator<ApplicationUser>[0],
-                  new Mock<ILookupNormalizer>().Object,
-                  new Mock<IdentityErrorDescriber>().Object,
-                  new Mock<IServiceProvider>().Object,
-                  new Mock<ILogger<UserManager<ApplicationUser>>>().Object)
-        {
+//    public class FakeUserManager : UserManager<ApplicationUser>
+//    {
+//        public FakeUserManager() : base(new Mock<IUserStore<ApplicationUser>>().Object,
+//                  new Mock<IOptions<IdentityOptions>>().Object,
+//                  new Mock<IPasswordHasher<ApplicationUser>>().Object,
+//                  new IUserValidator<ApplicationUser>[0],
+//                  new IPasswordValidator<ApplicationUser>[0],
+//                  new Mock<ILookupNormalizer>().Object,
+//                  new Mock<IdentityErrorDescriber>().Object,
+//                  new Mock<IServiceProvider>().Object,
+//                  new Mock<ILogger<UserManager<ApplicationUser>>>().Object)
+//        {
+//
+//        }
+//    }
 
-        }
-    }
-
-    public class FakeSignInManager : SignInManager<ApplicationUser>
-    {
-        public FakeSignInManager() : base(new FakeUserManager(),
-            new Mock<IHttpContextAccessor>().Object,
-            new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>().Object,
-            new Mock<IOptions<IdentityOptions>>().Object,
-            new Mock<ILogger<SignInManager<ApplicationUser>>>().Object,
-            new Mock<IAuthenticationSchemeProvider>().Object)
-        {
-
-        }
-    }
+//    public class FakeSignInManager : SignInManager<ApplicationUser>
+//    {
+//        public FakeSignInManager() : base(new FakeUserManager(),
+//            new Mock<IHttpContextAccessor>().Object,
+//            new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>().Object,
+//            new Mock<IOptions<IdentityOptions>>().Object,
+//            new Mock<ILogger<SignInManager<ApplicationUser>>>().Object,
+//            new Mock<IAuthenticationSchemeProvider>().Object)
+//        {
+//
+//        }
+//    }
 }
