3bc8c71129b3c1788bcd8ddb346520d0decbf330
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2018-11-27 03:32:51 +0300

    JWT(JSON Web Token) Authentication https://medium.com/@ozgurgul/asp-net-core-2-0-webapi-jwt-authentication-with-identity-mysql-3698eeba6ff8

--- ClubSystem.Api/Controllers/UserController.cs
+++ ClubSystem.Api/Controllers/UserController.cs
@@ -1,8 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 using ClubSystem.Lib.Model;
 using ClubSystem.Lib.Model.User;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ClubSystem.Api.Controllers
 {
@@ -11,50 +20,53 @@ namespace ClubSystem.Api.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _configuration;
 
-        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
+            IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _configuration = configuration;
         }
 
-        [HttpGet]
+        [HttpGet, Authorize]
         public IActionResult GetAllUsers()
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
 
-            return Ok(_userManager.Users);
+            return Ok(_userManager.Users.ToList());
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] User user)
+        public async Task<object> Login([FromBody] User user) // TODO: LoginDto model
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
 
-            var loginUser = new ApplicationUser
-            {
-                UserName = user.Username,
-                PasswordHash = user.Password
-            };
+            var result = await _signInManager.PasswordSignInAsync(user.Username, user.Password, false, false);
 
-            var result =
-                await _signInManager.PasswordSignInAsync(loginUser.UserName, loginUser.PasswordHash, false, false);
+            if (!result.Succeeded) throw new ApplicationException("INVALID_LOGIN_ATTEMPT");
 
-            if (result.Succeeded)
-                return Ok(result);
-
-            return BadRequest(ModelState);
+            var appUser = _userManager.Users.SingleOrDefault(r => r.Email == user.Email);
+            return GenerateJwtToken(user.Email, appUser);
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddUser([FromBody] User user)
+        public async Task<object> Register([FromBody] User user) // TODO: RegisterDto model
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
 
-            var newUser = new ApplicationUser {UserName = user.Username, Email = user.Email};
+            var newUser = new ApplicationUser
+            {
+                UserName = user.Username, Email = user.Email
+            };
             var result = await _userManager.CreateAsync(newUser, user.Password);
 
-            if (result.Succeeded) return Ok(result);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(newUser, false);
+                return GenerateJwtToken(user.Email, newUser);
+            }
 
             foreach (var responseError in result.Errors)
             {
@@ -62,6 +74,31 @@ namespace ClubSystem.Api.Controllers
             }
 
             return BadRequest(ModelState);
+        }
+
+        // TODO: Extension metoda çevirilebilir
+        private string GenerateJwtToken(string email, IdentityUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(Convert.ToDouble(_configuration["JwtExpireDays"]));
+
+            var token = new JwtSecurityToken(
+                _configuration["JwtIssuer"],
+                _configuration["JwtIssuer"],
+                claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
--- ClubSystem.Api/Startup.cs
+++ ClubSystem.Api/Startup.cs
@@ -1,14 +1,18 @@
-﻿using ClubSystem.Lib;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using ClubSystem.Lib;
 using ClubSystem.Lib.Interfaces;
 using ClubSystem.Lib.Model;
 using ClubSystem.Lib.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
@@ -23,7 +27,6 @@ namespace ClubSystem.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ClubSystemDbContext>(options =>
@@ -38,9 +41,28 @@ namespace ClubSystem.Api
                     b => b.MigrationsAssembly("ClubSystem.Api")
                 ));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
-                .AddDefaultTokenProviders();
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddEntityFrameworkStores<ApplicationIdentityDbContext>();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(config =>
+                {
+                    config.RequireHttpsMetadata = false;
+                    config.SaveToken = true;
+                    config.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = Configuration["JwtIssuer"],
+                        ValidAudience = Configuration["JwtIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
+                        ClockSkew = TimeSpan.Zero // remove delay of token when expire
+                    };
+                });
 
             services.AddMvc()
                 .AddJsonOptions(options =>
@@ -50,7 +72,6 @@ namespace ClubSystem.Api
                 })
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IClubRepository, ClubRepository>();
         }
 
@@ -67,7 +88,7 @@ namespace ClubSystem.Api
             }
 
             app.UseAuthentication();
-            
+
             app.UseCors(builder => builder
                 .AllowAnyHeader()
                 .AllowAnyMethod()
