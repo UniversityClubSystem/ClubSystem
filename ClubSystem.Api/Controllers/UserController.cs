ea7ac7ff3c922112e4621ca61752c550555828a2
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2018-11-27 23:34:07 +0300

    GenerateJwtToken refactor and UserController tests

--- ClubSystem.Api/Controllers/UserController.cs
+++ ClubSystem.Api/Controllers/UserController.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using ClubSystem.Lib.Model;
 using ClubSystem.Lib.Model.User;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ClubSystem.Api.Extensions;
 using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 
 namespace ClubSystem.Api.Controllers
 {
@@ -20,14 +15,14 @@ namespace ClubSystem.Api.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly IConfiguration _configuration;
+        private readonly JwtTokenGenerator _jwtTokenGenerator;
 
-        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
-            IConfiguration configuration)
+        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
-            _configuration = configuration;
+
+            _jwtTokenGenerator = new JwtTokenGenerator(configuration);
         }
 
         [HttpGet, Authorize]
@@ -48,7 +43,7 @@ namespace ClubSystem.Api.Controllers
             if (!result.Succeeded) return BadRequest(ModelState);
 
             var appUser = _userManager.Users.SingleOrDefault(r => r.UserName == user.UserName);
-            return GenerateJwtToken(user.UserName, appUser);
+            return _jwtTokenGenerator.GenerateJwtToken(user.UserName, appUser);
         }
 
         [HttpPost]
@@ -65,7 +60,7 @@ namespace ClubSystem.Api.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(newUser, false);
-                return GenerateJwtToken(user.UserName, newUser);
+                return _jwtTokenGenerator.GenerateJwtToken(user.UserName, newUser);
             }
 
             foreach (var responseError in result.Errors)
@@ -77,28 +72,28 @@ namespace ClubSystem.Api.Controllers
         }
 
         // TODO: Extension metoda çevirilebilir
-        private string GenerateJwtToken(string username, IdentityUser user)
-        {
-            var claims = new List<Claim>
-            {
-                new Claim(JwtRegisteredClaimNames.Sub, username),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(ClaimTypes.NameIdentifier, user.Id)
-            };
+        //private string GenerateJwtToken(string username, IdentityUser user)
+        //{
+        //    var claims = new List<Claim>
+        //    {
+        //        new Claim(JwtRegisteredClaimNames.Sub, username),
+        //        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        //        new Claim(ClaimTypes.NameIdentifier, user.Id)
+        //    };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtKey"]));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var expires = DateTime.Now.AddDays(Convert.ToDouble(_configuration["JwtExpireDays"]));
+        //    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtKey"]));
+        //    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        //    var expires = DateTime.Now.AddDays(Convert.ToDouble(_configuration["JwtExpireDays"]));
 
-            var token = new JwtSecurityToken(
-                _configuration["JwtIssuer"],
-                _configuration["JwtIssuer"],
-                claims,
-                expires: expires,
-                signingCredentials: creds
-            );
+        //    var token = new JwtSecurityToken(
+        //        _configuration["JwtIssuer"],
+        //        _configuration["JwtIssuer"],
+        //        claims,
+        //        expires: expires,
+        //        signingCredentials: creds
+        //    );
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
+        //    return new JwtSecurityTokenHandler().WriteToken(token);
+        //}
     }
 }
---
+++ ClubSystem.Test/UserControllerTests.cs
@@ -0,0 +1,88 @@
+﻿using ClubSystem.Api.Controllers;
+using ClubSystem.Lib.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Xunit;
+using ClubSystem.Lib.Model.User;
+using Microsoft.Extensions.Options;
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+
+namespace ClubSystem.Test
+{
+    public class UserControllerTests
+    {
+        private Mock<FakeUserManager> _mockUserManager;
+        private Mock<FakeSignInManager> _mockSignInManager;
+        private UserController _userController;
+
+        public UserControllerTests()
+        {
+            _mockUserManager = new Mock<FakeUserManager>();
+            _mockSignInManager = new Mock<FakeSignInManager>();
+        }
+
+        [Fact]
+        public async void UserCanLogin()
+        {
+            // Arrange
+            var userStoreMock = new Mock<IUserStore<ApplicationUser>>(); // ApplicationUser => User
+
+            _mockUserManager = new Mock<UserManager<ApplicationUser>>(userStoreMock.Object,
+                null, null, null, null, null, null, null, null);
+
+            var contextAccessor = new Mock<IHttpContextAccessor>();
+            var userPrincipalFactory = new Mock<IUserClaimsPrincipalFactory<User>>();
+
+            _mockSignInManager = new Mock<SignInManager<ApplicationUser>>(_mockUserManager.Object,
+                contextAccessor.Object, userPrincipalFactory.Object, null, null, null);
+
+
+            var mockConfiguration = new Mock<IConfiguration>();
+            var mockUser = new User
+            {
+                UserName = "user1",
+                Email = "mail@mail.com",
+                Password = "Ultra_Secure2Password!"
+            };
+            
+            // Act
+            var result = await _userController.Login(mockUser);
+
+            // Assert
+            Assert.IsType("".GetType(), result.GetType());
+        }
+    }
+
+    public class FakeUserManager : UserManager<ApplicationUser>
+    {
+        public FakeUserManager() : base(new Mock<IUserStore<ApplicationUser>>().Object,
+                  new Mock<IOptions<IdentityOptions>>().Object,
+                  new Mock<IPasswordHasher<ApplicationUser>>().Object,
+                  new IUserValidator<ApplicationUser>[0],
+                  new IPasswordValidator<ApplicationUser>[0],
+                  new Mock<ILookupNormalizer>().Object,
+                  new Mock<IdentityErrorDescriber>().Object,
+                  new Mock<IServiceProvider>().Object,
+                  new Mock<ILogger<UserManager<ApplicationUser>>>().Object)
+        {
+
+        }
+    }
+
+    public class FakeSignInManager : SignInManager<ApplicationUser>
+    {
+        public FakeSignInManager() : base(new FakeUserManager(),
+            new Mock<IHttpContextAccessor>().Object,
+            new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>().Object,
+            new Mock<IOptions<IdentityOptions>>().Object,
+            new Mock<ILogger<SignInManager<ApplicationUser>>>().Object,
+            new Mock<IAuthenticationSchemeProvider>().Object)
+        {
+
+        }
+    }
+}
